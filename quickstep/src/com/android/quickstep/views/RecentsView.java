4160b7ba1eca6d3cfc2de92d06516c4162f549cd
Vachounet <vachounet@live.fr>
2020-10-26 17:05:18 +0100

    Trebuchet: Move clear all button to actions view
    [AndyCGYan: Instead of removing the original, hide it in a simple way]
    
    Change-Id: I7a0b2729c163169f7606ad4644870d07d5165e8e

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -126,6 +126,7 @@ import android.view.ViewTreeObserver.OnScrollChangedListener;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.Interpolator;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.OverScroller;
 import android.widget.Toast;
@@ -546,6 +547,7 @@ public abstract class RecentsView<
     private final RecentsModel mModel;
     private final int mSplitPlaceholderSize;
     private final int mSplitPlaceholderInset;
+    private Button mActionClearAllButton;
     private final ClearAllButton mClearAllButton;
     private final Rect mClearAllButtonDeadZoneRect = new Rect();
     private final Rect mTaskViewDeadZoneRect = new Rect();
@@ -1165,6 +1167,8 @@ public abstract class RecentsView<
         mActionsView.updateFor3pLauncher(!supportsAppPairs());
         mSplitSelectStateController = splitController;
         mDesktopRecentsTransitionController = desktopRecentsTransitionController;
+        mActionClearAllButton = (Button) mActionsView.findViewById(R.id.action_clear_all);
+        mActionClearAllButton.setOnClickListener(this::dismissAllTasks);
     }
 
     public SplitSelectStateController getSplitSelectController() {
@@ -1596,7 +1600,7 @@ public abstract class RecentsView<
      * button fully visible, center page is Clear All button.
      */
     public boolean isClearAllHidden() {
-        return mClearAllButton.getAlpha() != 1f;
+        return true;
     }
 
     @Override
